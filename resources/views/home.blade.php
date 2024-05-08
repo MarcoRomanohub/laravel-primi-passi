c78dc9a413173e7b34d6370141674b35b9618ec1
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-08 15:51:58 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+
+<body>
+
+    <div class="container my-5 ">
+        <h1>Ciao Laravel</h1>
+        <h1>Home</h1>
+    </div>
+
+</body>
+
+</html>
